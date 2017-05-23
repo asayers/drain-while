d16be565a46c60dbcec1dc59b5b114bfe7098108
Alex Sayers <alex.sayers@gmail.com>
2017-05-23 15:08:10 +0900

    Benchmark an alternative implementation

--- benches/all.rs
+++ benches/all.rs
@@ -2,8 +2,11 @@ extern crate drain_while;
 extern crate easybench;
 
 mod dw;
+mod dw2;
 
 fn main() {
     println!("### drain_while ######");
     dw::bench();
+    println!("### drain_while_2 ####");
+    dw2::bench();
 }
---
+++ benches/dw2.rs
@@ -0,0 +1,40 @@
+use easybench::*;
+use std::iter::Peekable;
+use std::vec::Drain;
+
+pub struct DrainWhile<'a, T: 'a, P> {
+    inner: Peekable<Drain<'a,T>>,
+    pred: P,
+}
+
+impl<'a,T,P> Iterator for DrainWhile<'a,T,P> where P: Fn(&T) -> bool {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        let ok = if let Some(x) = self.inner.peek() {
+            (self.pred)(x)
+        } else { return None; };
+        if ok { self.inner.next() } else { None }
+    }
+}
+
+impl<'a,T,P> DrainWhile<'a,T,P> {
+    pub fn from_vec(x: &'a mut Vec<T>, pred: P) -> DrainWhile<T,P> where P: Fn(&T) -> bool {
+        DrainWhile { inner: x.drain(..).peekable(), pred: pred }
+    }
+}
+
+pub fn bench() {
+    let vec = (0..100).into_iter().zip((0..100).into_iter()).collect::<Vec<(usize,usize)>>();
+    fn bench_fn<F>(n: usize, f: F, xs: &mut Vec<(usize,usize)>)
+            where F: Fn(&(usize,usize)) -> bool {
+        let mut t = 0;
+        for (_,x) in DrainWhile::from_vec(xs, f) { t += x; }
+        assert_eq!(t, n);
+    }
+
+    println!("none     {}", bench_env(vec.clone(), |xs| bench_fn(0,    |&(_,_)| false , xs)));
+    println!("25 fired {}", bench_env(vec.clone(), |xs| bench_fn(300,  |&(x,_)| x < 25, xs)));
+    println!("50 fired {}", bench_env(vec.clone(), |xs| bench_fn(1225, |&(x,_)| x < 50, xs)));
+    println!("75 fired {}", bench_env(vec.clone(), |xs| bench_fn(2775, |&(x,_)| x < 75, xs)));
+    println!("all      {}", bench_env(vec.clone(), |xs| bench_fn(4950, |&(_,_)| true  , xs)));
+}
